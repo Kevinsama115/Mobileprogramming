7f775d0c6e9246ebd792c7cdb646de20ae0a2ede
Kevin <118007523+Kevinsama115@users.noreply.github.com>
2026-03-18 00:06:07 +0900

    Add files via upload

---
+++ Javacoding/week3/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Button button = new Button();
+        button.setClickListener(() -> System.out.println("Button clicked!"));
+        button.click();
+    }
+}
